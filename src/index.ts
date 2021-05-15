127546b86a64fa11444be87201f9e522e81a9285
Mehmet Onur Sagir <onuur.s@gmail.com>
2021-05-15 02:10:49 +0200

    feat: decorateRequest function

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client';
+import { FastifyPluginCallback } from 'fastify';
+
+export const decorateRequest: FastifyPluginCallback = (fastify, options, done) => {
+  const prisma = new PrismaClient();
+  const name = (options as any).name || 'prisma';
+
+  fastify.decorateRequest(name, prisma);
+
+  done();
+};
